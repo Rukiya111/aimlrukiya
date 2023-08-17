4e1f90eb319f8837e911c54755dae6c3981594aa
Rukiya <Rukiya123@gmail.com>
2023-08-17 14:35:58 +0530

    change2

---
+++ python2.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6]])
+print("Array is of type : ",type(arr))
+print("No of dimensions : ",arr.ndim)
+print("Shape of array :",arr.shape)
+print("size of  array :",arr.size)
+print("Array stores elements of type :",arr.dtype)
