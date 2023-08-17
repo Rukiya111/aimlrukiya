15c2ecd694306275b3b044b7806b0c01d834fc61
Rukiya <Rukiya123@gmail.com>
2023-08-17 15:45:27 +0530

    change5

---
+++ python5.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr1=np.array([1,2,3])
+arr2=np.array([4,5,6])
+print("Sum of 2 Arrays = ",arr1+arr2)
+print("Subtraction of 2 Arrays = ",arr1-arr2)
+print("Multiplication of 2 Arrays = ",arr1*arr2)
+print("Division of 2 Arrays = ",arr1/arr2)
+
