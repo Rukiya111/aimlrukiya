12318cb9d847d5d4cc8c86f0aa95bc751e95b335
Rukiya111 <141913155+Rukiya111@users.noreply.github.com>
2023-08-21 16:32:48 +0530

    Update python10.py

--- python10.py
+++ python10.py
@@ -2,7 +2,7 @@ import pandas as pd
 data=pd.DataFrame([[11,12,13,14],[13,14,15,16]],columns=["Maths","Science","social","hindi"])
 print(data)
 
-#Display in Name and age using dictionary
+#Displaying Name and age using dictionary
 import pandas as pd
 data={'Name':['Rukiya', 'Anudra', 'Poorna', 'Neha'],
         'Age':[17, 16, 18, 19]}
