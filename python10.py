dd685b7928edd4fdb00ddcae70c03a9297a21d99
Kushalbg <kushalbandi8@gmail.com>
2023-08-21 16:31:13 +0530

    change10

---
+++ python10.py
@@ -0,0 +1,13 @@
+import pandas as pd
+data=pd.DataFrame([[11,12,13,14],[13,14,15,16]],columns=["Maths","Science","social","hindi"])
+print(data)
+
+#Display in Name and age using dictionary
+import pandas as pd
+data={'Name':['Rukiya', 'Anudra', 'Poorna', 'Neha'],
+        'Age':[17, 16, 18, 19]}
+
+p=pd.DataFrame(data)
+print(p)
+
+
